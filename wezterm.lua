0de29ac64f08cd25862233c48d8984fe8a584e06
rafael-ae <rafael.gutierrez_c@aescape.com>
2024-07-26 17:29:49 -0400

    update dotfiles

--- wezterm.lua
+++ wezterm.lua
@@ -5,8 +5,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.enable_scroll_bar = true
--- config.font = wezterm.font 'BlexMono Nerd Font'
+-- config.font = wezterm.font 'MesloLGS Nerd Font Mono'
 config.color_scheme = 'TokyoNight'
+-- config.color_scheme = 'Chalk (dark) (terminal.sexy)'
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
