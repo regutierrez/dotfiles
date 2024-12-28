c71fd00b802a0b1203e58c9e63d9ee1bad4afd11
paelgutierrez <11924623+regutierrez@users.noreply.github.com>
2024-12-28 18:42:14 +0800

    cleanup repo

--- add_dotfiles.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-ln -s alacritty.toml $HOME/.config/alacritty/alacritty.toml
-# ln -s karabiner.json $HOME/.config/karabiner/karabiner.json
-ln -s tmux.conf $HOME/.tmux.conf
-# ln -s zshrc $HOME/.zshrc
