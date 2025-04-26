461bc464c8ab1bb93e4643577666758f1f88e99f
regutierrez <11924623+regutierrez@users.noreply.github.com>
2025-04-25 21:35:38 -0400

    update configs

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,12 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 config.enable_scroll_bar = true
 -- config.font = wezterm.font 'MesloLGS Nerd Font Mono'
-config.color_scheme = 'TokyoNight'
+-- config.color_scheme = 'TokyoNight'
 -- config.color_scheme = 'Chalk (dark) (terminal.sexy)'
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
